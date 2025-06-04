73dc9a5be410eb58314a8f70fbf1359ab9eeeaa7
Omegea Hunter <omegeadhunter@gmail.com>
2025-06-04 17:58:47 -0400

    Add functions to calculate room areas and total area in e2.js

--- exercises/e2.js
+++ exercises/e2.js
@@ -11,15 +11,18 @@
 */
 
 // Your function code starts here...
-
+function rectArea(sideA, sideB){
+    return sideA * sideB;
+}
 
 // Use the function you created above to calculate the areas of 3 rooms
 // Assign the results to room1, room2, and room3 variables
 // Data for calculation: room1: 4 by 6, room2: 13 by 3, room3: 4 by 8
-
 // Your code goes here:
-
-
+let room1 = rectArea(4, 6), room2 = rectArea(13, 3), room3 = rectArea(4, 8);
+console.log (`The area of room1 is ${room1}`);
+console.log (`The area of room2 is ${room2}`);
+console.log (`The area of room3 is ${room3}`);
 
 /**
  * =============================================================
@@ -32,16 +35,21 @@
  */
 
 // Your code goes here...
+function totalRoomsArea (area1, area2, area3) {
+    return area1 + area2 + area3;
+}
+
+
 
 
 
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-    console.log(/* replace this comment with a function call here... */);
-}
+    console.log(totalRoomsArea(24, 35, 24));
+};
 
-
+console.log(result());
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
